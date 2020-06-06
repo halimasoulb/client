43ab9a4c54cf50fa0d08a3c437c19253d556a973
Halima SOULB <halimasoulb@yahoo.com>
2020-06-06 03:49:31 +0100

    hhgghg

--- serve.js
+++ serve.js
@@ -1,9 +1,9 @@
 const express=require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname+'./dist/data-amine'));
+app.use(express.static(__dirname+'/dist/dataAmine'));
 app.get('/*',function(req,res){
 
-    res.sendfile(path.join(__dirname+'./dist/data-amine/index.html'));
+    res.sendfile(path.join(__dirname+'/dist/dataAmine/index.html'));
 })
 app.listen(process.env.PORT || 8088);
